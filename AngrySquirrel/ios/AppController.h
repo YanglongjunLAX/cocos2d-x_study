73d2b51ad1230ffb2c4afe4af3d24c6ca740a0cf
xue <xue@xuematoiMac.local>
2013-05-19 16:46:42 +0800

    Initial Commit

---
+++ AngrySquirrel/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  AngrySquirrelAppController.h
+//  AngrySquirrel
+//
+//  Created by xue on 13-5-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ AngrySquirrel/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  AngrySquirrelAppController.h
+//  AngrySquirrel
+//
+//  Created by xue on 13-5-19.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
